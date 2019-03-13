1086a423c231c72b436d222b3efdde0cf2e9d7db
Soh1121 <satou.shg@gmail.com>
2019-03-13 14:32:33 +0900

    [update]スタイルシートを指定

--- wp-content/themes/mytheme/index.php
+++ wp-content/themes/mytheme/index.php
@@ -5,6 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>ページタイトル</title>
+    <link rel="stylesheet" href="<?php echo get_stylesheet_uri(); ?>">
 </head>
 <body <?php body_class(); ?>>
 
