91527a99b93e2187a1a31ccbad1206f41ce9fcc2
Soh1121 <satou.shg@gmail.com>
2019-03-18 23:47:42 +0900

    [update]サイト名をページのタイトルにする

--- wp-content/themes/mytheme/index.php
+++ wp-content/themes/mytheme/index.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>ページタイトル</title>
+    <title><?php bloginfo('name'); ?></title>
     
     <link href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet">
     <link rel="stylesheet" href="<?php echo get_stylesheet_uri(); ?>">
