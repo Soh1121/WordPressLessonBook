8f671d4cd595ac9668614b64820fd0ba83d45cd6
Soh1121 <satou.shg@gmail.com>
2019-03-18 23:49:46 +0900

    [update]ページタイトルに記事のタイトルを入れる

--- wp-content/themes/mytheme/index.php
+++ wp-content/themes/mytheme/index.php
@@ -4,7 +4,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title><?php bloginfo('name'); ?></title>
+    <title><?php bloginfo('name'); ?>
+    <?php wp_title(); ?></title>
     
     <link href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet">
     <link rel="stylesheet" href="<?php echo get_stylesheet_uri(); ?>">
