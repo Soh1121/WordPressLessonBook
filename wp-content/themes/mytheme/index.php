74ff2877cca4d6414333295591af5f590d31b34b
Soh1121 <satou.shg@gmail.com>
2019-03-22 23:36:32 +0900

    [update/fix]h1にclass名が付いていなかったのを修正、見出しのデザインを指定（１）

--- wp-content/themes/mytheme/index.php
+++ wp-content/themes/mytheme/index.php
@@ -28,8 +28,10 @@
 <div class="container">
 
 <?php if(is_category()): ?>
+<h1 class="archive-title">
 <i class="fa fa-folder-open"></i>
-<h1>「<?php single_cat_title(); ?>」に関する記事</h1>
+「<?php single_cat_title(); ?>」に関する記事
+</h1>
 <?php endif; ?>
 
 <?php if(have_posts()): while(have_posts()): the_post(); ?>
