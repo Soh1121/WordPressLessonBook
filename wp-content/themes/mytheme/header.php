3fa146bcb0674cf74e6ad4d7ac48a5525263abbc
Soh1121 <satou.shg@gmail.com>
2019-03-29 21:28:42 +0900

    [update]スマートフォン・タブレットにも適応

--- wp-content/themes/mytheme/header.php
+++ wp-content/themes/mytheme/header.php
@@ -7,6 +7,8 @@
     <title><?php bloginfo('name'); ?>
     <?php wp_title(); ?></title>
 
+    <meta name="viewport" content="width=device-width">
+
     <link href="https://fonts.googleapis.com/css?family=Acme" rel="stylesheet">
     
     <link href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet">
