8b01a6cac93cf0a3f2d21e276403c7d17b22a319
Soh1121 <satou.shg@gmail.com>
2019-03-29 22:02:31 +0900

    [update]デフォルトのベッダー画像の指定とテキストの削除を行う

--- wp-content/themes/mytheme/functions.php
+++ wp-content/themes/mytheme/functions.php
@@ -12,5 +12,7 @@ register_nav_menu('navigation', 'ナビゲーション');
 // カスタムヘッダー
 add_theme_support('custom-header', array(
     'width' => 1500,
-    'height' => 250
+    'height' => 250,
+    'default-image' => '%s/header-1500x250.jpg',
+    'header-text' => false
 ));
