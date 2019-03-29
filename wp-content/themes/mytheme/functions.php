f27065b8ab0c71d4fba8de6f6815dd658f41ee46
Soh1121 <satou.shg@gmail.com>
2019-03-29 22:56:56 +0900

    [update]概要の省略記号を変更する

--- wp-content/themes/mytheme/functions.php
+++ wp-content/themes/mytheme/functions.php
@@ -18,7 +18,13 @@ add_theme_support('custom-header', array(
 ));
 
 // 概要（抜粋）の文字数
-//function my_length($length) {
-//    return 70;
-//}
-//add_filter('excerpt_mblength', 'my_length');
+function my_length($length) {
+    return 70;
+}
+add_filter('excerpt_mblength', 'my_length');
+
+// 概要（抜粋）の省略記号
+function my_more($more) {
+    return '…';
+}
+add_filter('excerpt_more', 'my_more');
